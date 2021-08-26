c1dfc58b05202fc8d9d69c395998a38b4a5412b1
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-08-26 23:42:59 +0000

    F!! Done
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- src/main/java/org/samples/GameOfLife.java
+++ src/main/java/org/samples/GameOfLife.java
@@ -1,5 +1,7 @@
 package org.samples;
 
+import org.lambda.query.Query;
+import org.lambda.query.Queryable;
 import org.lambda.utils.Grid;
 
 import java.awt.*;
@@ -16,16 +18,62 @@ public class GameOfLife {
     }
 
     public void advance() {
-        board = new ArrayList<>();
-        changes = new ArrayList<>();
+        var newBoard = new ArrayList<Point>();
+        newBoard.addAll(board);
+        //find all the changes and their neighbors
+        final ArrayList<Point> newChanges = new ArrayList<>();
+        Queryable<Queryable<Point>> select = Query.select(changes, c -> getNeighbours(c));
+        Queryable<Point> flatten = flatten(select);
+        for (Point point : flatten) {
+            int neighbours = countNeighbours(point);
+            if (!isAlive(point) && neighbours == 3) {
+                newChanges.add(point);
+                newBoard.add(point);
+            } else if (isAlive(point) && !(neighbours == 2 || neighbours == 3)) {
+                newBoard.remove(point);
+                newChanges.add(point);
+            }
+        }
+        board = newBoard;
+        changes = newChanges;
+    }
+
+    private int countNeighbours(Point point) {
+        return getNeighbours(point).where(p -> !p.equals(point) && isAlive(p)).size();
+    }
+
+    private boolean isAlive(Point point) {
+        return isAlive(point.x, point.y);
+    }
+
+    private Queryable<Point> flatten(Queryable<Queryable<Point>> select) {
+        Queryable<Point> flattenedList = new Queryable<>(Point.class);
+        for (Queryable<Point> points : select) {
+            flattenedList.addAll(points);
+        }
+        return flattenedList;
+    }
+
+    private Queryable<Point> getNeighbours(Point c) {
+        return Queryable.as(
+                new Point(c.x - 1, c.y - 1),
+                new Point(c.x - 0, c.y - 1),
+                new Point(c.x + 1, c.y - 1),
+                new Point(c.x - 1, c.y - 0),
+                new Point(c.x - 0, c.y - 0),
+                new Point(c.x + 1, c.y - 0),
+                new Point(c.x - 1, c.y + 1),
+                new Point(c.x - 0, c.y + 1),
+                new Point(c.x + 1, c.y + 1)
+        );
     }
 
     @Override
     public String toString() {
-        return Grid.print(5,7,(x,y)-> isAlive(x,y) ? "X":"_");
+        return Grid.print(10, 10, (x, y) -> isAlive(x, y) ? "X" : "_");
     }
 
     private boolean isAlive(Integer x, Integer y) {
-        return board.contains(new Point(x,y));
+        return board.contains(new Point(x, y));
     }
 }
--- src/test/java/org/samples/SampleTests.java
+++ src/test/java/org/samples/SampleTests.java
@@ -7,18 +7,31 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+public class SampleTests {
+    @Test
+    public void testSingleCellAlive() {
+        StoryBoard b = new StoryBoard();
+        GameOfLife g = new GameOfLife(new Point(1, 1));
+        b.add(g.toString());
+        g.advance();
+        b.add(g.toString());
+        Approvals.verify(b);
+    }
 
-public class SampleTests
-{
-  @Test
-  public void testSingleCellAlive()
-  {
-    StoryBoard b = new StoryBoard();
-    GameOfLife g = new GameOfLife(new Point(1,1));
-    b.add(g.toString());
-    g.advance();
-    b.add(g.toString());
-    Approvals.verify(b);
-  }
+    @Test
+    public void testGlider() {
+        StoryBoard b = new StoryBoard();
+        GameOfLife g = new GameOfLife(new Point(1, 1),
+                 new Point(2, 2),
+                 new Point(3, 0),
+                 new Point(3, 1),
+                 new Point(3, 2)
+        );
+        b.add(g.toString());
+        for (int i = 0; i < 4; i++) {
+            g.advance();
+            b.add(g.toString());
+        }
+        Approvals.verify(b);
+    }
 }
